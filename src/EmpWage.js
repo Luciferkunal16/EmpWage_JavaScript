5fb9aae055316e8e024794138e7ef4039df974ae
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-11-30 19:32:11 +0530

    Added UC 3: Refactor the program and use Function to compute Empwage

--- src/EmpWage.js
+++ src/EmpWage.js
@@ -3,19 +3,24 @@ const IS_PART_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
-let empHour=0;
-empCheck=Math.floor(Math.random()*10)%3;
-switch(empCheck){
-    case IS_PART_TIME:
-        empHour=PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHour=FULL_TIME_HOURS;
-        break;
-    default:
-        empHour=0;
 
-
+function getWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+            break;
+        default:
+            return 0;
+    
+    
+    }
+    
 }
-let empWage=empHour*WAGE_PER_HOUR;
+let empHrs=0;
+empCheck=Math.floor(Math.random()*10)%3+1;
+empHrs=getWorkingHours(empCheck);
+let empWage=empHrs*WAGE_PER_HOUR;
 console.log(empWage);
